f436c88dee2f4b6566c70cd5dab2bfafa9cc32ac
aucfan-makise <makise@aucfan.com>
2016-04-26 14:34:06 +0900

    gitignore追加

--- vagrant/chef-repo/cookbooks/php-mysql-httpd/recipes/php70.rb
+++ vagrant/chef-repo/cookbooks/php-mysql-httpd/recipes/php70.rb
@@ -10,9 +10,22 @@ execute "Install remi repository" do
   not_if "rpm -qa | grep -q 'remi-release'"
 end
 
-%w{php php-mbstring php-pdo php-mysqlnd php-xml php-devel}.each do |name|
+%w{php php-mbstring php-pdo php-mysqlnd php-xml php-devel php-pecl-xdebug}.each do |name|
   package name do
     action :install
     options '--enablerepo=remi-php70'
   end
+
+template "/etc/php.d/php.local.ini" do
+  source "php.local.ini.erb"
+  owner "vagrant"
+  group "vagrant"
+end
+
+template "/etc/php.d/15-xdebug.ini" do
+    source "xdebug.ini.erb"
+    owner 'root'
+    group 'root'
+    mode 644
+end
 end
